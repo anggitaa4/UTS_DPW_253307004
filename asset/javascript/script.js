a86b34505443e19978ad417efa0ef6aca85a13ff
putriimendyy <putriimendy@gmail.com>
2026-04-15 15:57:53 +0700

    UTS DPW

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,29 +1,18 @@
-function showToast(pesan) {
-    const toast = document.getElementById("toast");
-    toast.innerText = pesan;
-    toast.style.opacity = 1;
-
-    setTimeout(() => {
-        toast.style.opacity = 0;
-    }, 3000);
-}
-
 function kirimData() {
     let nama = document.getElementById("nama").value;
     let nik = document.getElementById("nik").value;
     let jalur = document.getElementById("jalur").value;
 
     if (nama === "" || nik === "" || jalur === "") {
-        showToast("Semua field wajib diisi!");
+        alert("Semua field wajib diisi!");
         return;
     }
-
     if (nik.length !== 16) {
-        showToast("NIK harus 16 digit!");
+        alert("NIK harus 16 digit!");
         return;
     }
-
-    showToast("Pendaftaran berhasil!");
-
-    document.getElementById("formDaftar").reset();
+    document.getElementById("successPopup").style.display = "flex";
+}
+    function closePopup() {
+    document.getElementById("successPopup").style.display = "none";
 }
